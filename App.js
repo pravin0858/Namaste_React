c3cfe6df1bdb5a85a148b87881cefb96c7b72774
pravin0858 <111354875+pravin0858@users.noreply.github.com>
2023-10-14 21:50:28 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+
+
 const heading = React.createElement("div" , {id:"parent"},
   [React.createElement("div",{id:"child"},
   [React.createElement("h1",{},"I'm an h1 element"),React.createElement("h2",{},"i'm an h2 element")]),        React.createElement("div",{id:"child2"},

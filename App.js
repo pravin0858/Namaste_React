aaba883fd2d02ac2aa8a69f42927e9d279c704c1
pravin0858 <111354875+pravin0858@users.noreply.github.com>
2023-10-14 12:08:17 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("div" , {id:"parent"},
+  [React.createElement("div",{id:"child"},
+  [React.createElement("h1",{},"I'm an h1 element"),React.createElement("h2",{},"i'm an h2 element")]),        React.createElement("div",{id:"child2"},
+  [React.createElement("h1",{},"I'm an h1 element"),React.createElement("h2",{},"i'm an h2 element")])]
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
